76f84c60f666c385fc22543d8f3d235918a03b56
HomePC <jackie@dast.tw>
2024-05-10 01:08:36 +0800

    改進：整合新的語言伺服器和格式化工具
    - 在程式碼格式化插件中新增對「zsh」的支援
    - 整合「powershell_es」語言伺服器設定
    - 將「powershell_es」語言伺服器納入 LSP 插件中
    - 將「beautysh」添加到代碼格式化工具清單中
    - 擴大對 treesitter 插件支持的文件類型
    
    Signed-off-by: HomePC <jackie@dast.tw>

--- lua/dast/plugins/lsp/mason.lua
+++ lua/dast/plugins/lsp/mason.lua
@@ -30,18 +30,20 @@ return {
         "bashls",
         "lua_ls",
         "pyright",
+        "powershell_es",
       },
     })
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier",   -- Prettier is an opinionated code formatter.
-        "black",      -- Black, the uncompromising Python code formatter.
+        "prettier", -- Prettier is an opinionated code formatter.
+        "black", -- Black, the uncompromising Python code formatter.
         "shellcheck", --  ShellCheck, a static analysis tool for shell scripts.
-        "pylint",     -- Pylint is a static code analyser for Python 2 or 3.
-        "stylua",     -- An opinionated Lua code formatter.
-        "isort",      -- isort is a Python utility / library to sort imports alphabetically.
-        "shfmt",      -- A shell formatter (sh/bash/mksh).
+        "pylint", -- Pylint is a static code analyser for Python 2 or 3.
+        "stylua", -- An opinionated Lua code formatter.
+        "isort", -- isort is a Python utility / library to sort imports alphabetically.
+        "shfmt", -- A shell formatter (sh/bash/mksh).
+        "beautysh",
       },
     })
   end,
