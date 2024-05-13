66d8d8289004f4714c39a588136a3804d123e1e5
HomePC <jackie@dast.tw>
2024-05-13 14:55:24 +0800

    修正 ChatGPT 插件的配置
    - 新增 `chatgpt.lua` 插件及其配置設置
    - 包括 ChatGPT 指令的鍵映射
    
    Signed-off-by: HomePC <jackie@dast.tw>

---
+++ lua/dast/plugins/chatgpt.lua
@@ -0,0 +1,20 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  -- enabled = false,
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    local home = vim.fn.expand("$HOME")
+    require("chatgpt").setup({
+      api_key_cmd = "gpg --decrypt " .. home .. "/openai.gpg",
+    })
+    vim.keymap.set("n", "<Leader>cc", "<cmd>ChatGPT<CR>", { desc = "Open ChatGPT" })
+    vim.keymap.set("n", "<Leader>cC", "<cmd>CHatGPTCompleteCode<CR>", { desc = "ChatGPT AutoComplete Code" })
+    vim.keymap.set("n", "<Leader>ct", "<cmd>ChatGPTRun add_tests<CR>", { desc = "ChatGPT Add Test Code" })
+  end,
+}
