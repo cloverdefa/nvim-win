164b20ba571c35582a01982b59099bc1f16291ba
OfficePC <jackie@dast.tw>
2024-05-17 13:00:27 +0800

    GIT 提交消息：
    翻新 Lua/DAST 檔案的外掛配置
    
    - 註解掉 `lua/dast/plugins/chatgpt.lua` 檔案中的 `enabled` 欄位
    - 註解掉 `lua/dast/plugins/substitute.lua` 檔案中的 `enabled` 欄位
    
    Signed-off-by: OfficePC <jackie@dast.tw>

--- lua/dast/plugins/substitute.lua
+++ lua/dast/plugins/substitute.lua
@@ -1,6 +1,6 @@
 return {
   "gbprod/substitute.nvim",
-  enabled = false,
+  -- enabled = false,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local substitute = require("substitute")
