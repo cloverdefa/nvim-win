299de0aae0bc93dbc4ef3f722f77028246b11300
HomePC <jackie@dast.tw>
2024-05-28 20:31:19 +0800

    功能：在 Vim 插件中重新設計 session 管理
    - 新增一個新插件來管理 Vim session
    - 修改與 session 恢復有關的按鍵映射
    - 更新超時設定從 1000 到 500
    
    Signed-off-by: HomePC <jackie@dast.tw>

---
+++ lua/dast/plugins/auto-session.lua
@@ -0,0 +1,17 @@
+return {
+  "rmagatti/auto-session",
+  event = "VeryLazy",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- 恢復至上一個儲存的工作區
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- 將目前作業路徑儲存至工作區
+  end,
+}
