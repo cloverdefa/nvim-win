a99e69f959d277fca78d6b61eaa9bf992e0f0caf
HomePC <jackie@dast.tw>
2024-08-27 22:06:39 +0800

    refactor: refactor options handling in setup function
    - Remove the `event = "VeryLazy"` line
    - Add `lazy = false` to the `opts` object
    - Add `dependencies` array to the `opts` object
    - Add key mappings to the `keys` array in the `opts` object
    - Remove `auto_restore_enabled`, and `auto_session_suppress_dirs` settings
    - Add `auto_restore: false`, and `bypass_save_filetypes` to the `setup` function
    - Change `auto_session_suppress_dirs` to `suppress_dirs` in the `setup` function
    - Add `args_allow_files_auto_save` function to the `opts` object
    
    Signed-off-by: HomePC <jackie@dast.tw>

--- lua/dast/plugins/auto-session.lua
+++ lua/dast/plugins/auto-session.lua
@@ -1,18 +1,38 @@
 return {
   "rmagatti/auto-session",
-  event = "VeryLazy",
-  config = function()
-    local auto_session = require("auto-session")
-
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
-    })
-
-    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-    local keymap = vim.keymap
-
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- 恢復至上一個儲存的工作區
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- 將目前作業路徑儲存至工作區
-  end,
+  lazy = false,
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    keys = {
+      { "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+      { "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+      { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+    },
+    config = function()
+      local auto_session = require("auto-session")
+      auto_session.setup({
+        auto_restore = false,
+        bypass_save_filetypes = { "alpha", "dashboard" }, -- or whatever dashboard you use
+        suppress_dirs = { "~/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      })
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+    end,
+    args_allow_files_auto_save = function()
+      local supported = 0
+      local buffers = vim.api.nvim_list_bufs()
+      for _, buf in ipairs(buffers) do
+        -- Check if the buffer is valid and loaded
+        if vim.api.nvim_buf_is_valid(buf) and vim.api.nvim_buf_is_loaded(buf) then
+          local path = vim.api.nvim_buf_get_name(buf)
+          if vim.fn.filereadable(path) ~= 0 then
+            supported = supported + 1
+          end
+        end
+      end
+      -- If we have more 2 or more supported buffers, save the session
+      return supported >= 2
+    end,
+  },
 }
