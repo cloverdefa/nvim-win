ee2464d65c8668b65b651857f8754d5c43662cea
HomePC-WSL <jackie@dast.tw>
2024-09-05 23:21:12 +0800

    chore: refactor file paths across the project
    - Update the `allowed_dirs` path from `~/Documents/github` to `~/github`
    
    Signed-off-by: HomePC-WSL <jackie@dast.tw>

--- lua/dast/plugins/auto-session.lua
+++ lua/dast/plugins/auto-session.lua
@@ -12,7 +12,7 @@ return {
   opts = {
     auto_restore = false,
     bypass_save_filetypes = { "alpha", "dashboard" },
-    allowed_dirs = { "~/Documents/github" },
+    allowed_dirs = { "~/github" },
     suppress_dirs = { "~/", "~/Downloads", "~/Desktop/" },
   },
 }
