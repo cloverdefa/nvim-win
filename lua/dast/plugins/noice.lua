2307b6c3b9f1bb46d47220b264ce140750c89749
OfficePC-WSL <jackie@dast.tw>
2024-11-27 13:07:10 +0800

    feat: refactor Lua plugin configuration and keymap in `noice.lua`
    - Add new presets for easier configuration in `lua/dast/plugins/noice.lua`
    - Include a new keymap for telescope search message history in `lua/dast/plugins/noice.lua`
    
    Signed-off-by: OfficePC-WSL <jackie@dast.tw>

--- lua/dast/plugins/noice.lua
+++ lua/dast/plugins/noice.lua
@@ -12,10 +12,18 @@ return {
       },
       lsp = {
         override = {
-          -- 覆寫 LSP 的 markdown 處理方式，使其與 Noice 一起工作
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
       },
     })
     vim.keymap.set("n", "<leader>nc", function()
@@ -24,6 +32,9 @@ return {
     vim.keymap.set("n", "<leader>nl", function()
       require("noice").cmd("history")
     end, { desc = "shows the message history" })
+    vim.keymap.set("n", "<leader>nt", function()
+      require("noice").cmd("telescope")
+    end, { desc = "telescope search message history" })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
